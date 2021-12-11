2186fc90ffa769fb8d85738dfd3baf6a32ab886b
Ace Eldeib <alexeldeib@gmail.com>
2021-12-11 18:51:04 -0500

    more js changes

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 fetch('data.json')
     .then(response => response.json())
     .then(data => {
-        console.log(data);
+        // add more traces to plot same types of things against each other.
+        // add more charts in different divs to multiple things. 
         var trace1 = {
+            x: [],
+            y: [],
+            type: 'scatter'
+        };
+
+        var trace2 = {
+            x: [],
+            y: [],
+            type: 'scatter'
+        };
+
+        var trace3 = {
             x: [],
             y: [],
             type: 'scatter'
@@ -10,11 +23,19 @@ fetch('data.json')
 
         for (let i = 0; i < data.length; i++) {
             trace1.x.push(data[i].time)
+            trace2.x.push(data[i].time)
+            trace3.x.push(data[i].time)
+
             trace1.y.push(i+1)
+            trace2.y.push(i+100)
+            trace3.x.push(i*100)
         }
         console.log(trace1)
-        var traces = [trace1];
-        Plotly.newPlot('myDiv', traces);
+        var traces1 = [trace1, trace2];
+        var traces2 = [trace3]
+        Plotly.newPlot('time', traces1);
+        Plotly.newPlot('bigtime', traces2);
+
     })
     .catch(error => console.log(error));
 
